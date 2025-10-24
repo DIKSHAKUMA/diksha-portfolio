27c41d150dcef67a135007ab94f3ecb17c4124ff
Thomas <thomas.thorstensson@gmail.com>
2025-10-24 20:12:21 +0200

    Again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -92,7 +92,7 @@ export default defineNuxtConfig({
       {
         name: 'Lato',
         provider: 'google',
-        weights: [300, 400, 700],
+        weights: [300, 400, 500, 600, 700],
         styles: ['normal', 'italic'],
       },
       {
